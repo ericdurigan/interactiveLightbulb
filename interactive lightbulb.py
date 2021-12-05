3c541c21d8e9ad4d485a4c077c18d217cb750970
ericdurigan <89487642+ericdurigan@users.noreply.github.com>
2021-12-04 19:50:41 -0700

    Add files via upload

---
+++ interactive lightbulb.py
@@ -0,0 +1,75 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Nov 27 14:26:16 2021
+
+@author: ericd
+"""
+
+
+"""The goal is to make a lightbulb that could light up multiple colors"""
+
+import turtle
+from playsound import playsound
+
+
+
+# =================== LIBRARY SETTINGS SETUP =========================
+turtle.colormode(255) # accept 0-255 RGB values
+turtle.tracer(0) # turn off turtle's animation
+
+panel = turtle.Screen()
+w = 800
+h = 800
+panel.setup(width=w, height=h)
+panel.bgcolor("black")
+
+#CLASSES ARE NOUNS, METHODS ARE VERBS
+
+"""STEP 1- MAKE THE SHAPES IN CLASSES"""
+# CLASSES WE WILL USE:
+# 	A CLASS FOR THE CIRCLE- ERIC WILL MAKE A CIRCLE
+# 	A CLASS FOR THE RECTANGLES- TIM WILL MAKE THREE RECTANGLES
+# 	A CLASS FOR THE SOUND- SAI WILL DO THE SOUND
+
+"""THE THREE RECTANGLES BELOW THE CIRCLE WILL MAKE THE SHAPES AESTHETICALLY LOOK LIKE A LIGHTBULB"""
+	
+
+
+"""STEP #2- MAKING THE INTERACTIONS"""
+	
+# 	ERIC WILL MAKE THE LIGHTBULB CLICKABLE
+     
+
+"""STEP #3- MAKE THE COLORS"""
+	
+# 	ERIC WILL MAKE THE CIRCLE- ATLTERNATE BETWEEN [RED, GREEN, BLUE]
+# 	
+# 	TIM WILL MAKE THE RECTANGLES- GRAY
+
+"""PLAYBACK SOUND"""
+class playSound():
+    playsound('2.wav')
+    
+
+
+# ================ VARIABLE DEFINITION & SETUP =======================
+"""The objective here is to make an RGB lightbulb that is connected corresponding buttons"""
+
+"""MAKING THE LIGHTBULB"""
+
+
+
+
+
+
+
+
+turtle.done()
+
+
+
+
+
+
+
+
